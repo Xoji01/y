4454b8b1bab0418d310f6b7dbeffb46afee1cda2
Xoji 01 <skajsksjahahahah@gmail.com>
2023-09-09 14:21:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let name = 'Alex'
+let money = 10000
+let account = 7777
+ let aa = prompt('Как вас зовут?').trim().toLowerCase()
+ if (aa === 'alex') 
+    aas = prompt ('Код баланса')
+    else 
+    alert("Не правильно")
+    
+
+if (aas === '7777') {aaf =+prompt("Сколько обналичить?")} else {alert ('Не правильно')}
+if (aaf >=0   && aaf<=10000) { aq=alert ('Все отлично')
+ 
+alert('Вы обналичиловали ' + aaf)
+alert('У вас осталось ' +('10000' -aaf))
+} else{ alert ('У вас недостаточно средтсв')}
+
+let user = prompt ('name'). toLowerCase ().trim()
+let sv = Math. floor (Math. random () * 10)
+let mest = 10
+let bmest =mest-sv
+     alert (bmest + "свободные места ")
+   if (user[0]=== 'a')   
+        {let age = prompt ('Сколько вам лет?')
+       if (age>=20&&age<= 40) 
+{let dolar = prompt ('Сколько денег имеешь?')
+if (dolar>= 100) {
+let kolpe= prompt ( 'Сколько вас')
+if (kolpe > bmest ||kolpe < 0 ) {
+alert ("Занято")
+} 
+else {alert ("Добро пожаловать")}}}}
